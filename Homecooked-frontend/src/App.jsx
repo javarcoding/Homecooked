c152b09b603f8b22e1bf474ae76166c19ea92a3b
javarcoding <ravirajadsul9297@gmail.com>
2025-12-07 08:57:14 +0530

    Installed Tailwind, Redux Toolkit, React Router, Axios and initial setup

--- Homecooked-frontend/src/App.jsx
+++ Homecooked-frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div><h1>Homecooked Frontend Initialized</h1>;</div>
+      <AppRoutes />;
     </>
   )
 }
---
+++ Homecooked-frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="text-3xl font-bold p-5">
+            Homecooked Frontend Ready ✔️
+        </div>
+    )
+}
