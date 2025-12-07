ba231377fa8a2bc9497fa15c3254e58c62c40979
javarcoding <ravirajadsul9297@gmail.com>
2025-12-07 10:00:58 +0530

    Initialized Spring Boot backend with JPA, MySQL, Swagger

---
+++ Homecooked-backend/Homecooked/src/main/java/com/homecooked/homecooked/HomecookedApplication.java
@@ -0,0 +1,13 @@
+package com.homecooked.homecooked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecookedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecookedApplication.class, args);
+	}
+
+}
---
+++ Homecooked-backend/Homecooked/src/test/java/com/homecooked/HomecookedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecooked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecookedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
